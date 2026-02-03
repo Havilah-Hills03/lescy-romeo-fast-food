98810fe1893dfbf4e315e8d40239eaf6750c7210
Jerry Jakes <jerryjake227@gmail.com>
2026-02-03 19:19:23 +0100

    updated the car responsiveness

--- app/Hero-page-sections/Top-Product.tsx
+++ app/Hero-page-sections/Top-Product.tsx
@@ -73,7 +73,7 @@ const TopProduct: React.FC = () => {
   <h1 className="h1 text-center pt-12 font-medium text-[20px] lg:text-[35px] md:text-[35px]" 
   >Top Products</h1>
      <div 
-      className=" max-w-4xl lg:max-w-8xl  md:max-w-7xl  flex justify-center items-center h-[50vh] lg:h-screen px-1"
+      className=" max-w-4xl lg:max-w-8xl  md:max-w-7xl  flex justify-center items-center h-[55vh] lg:h-screen px-1"
       
     >
       
