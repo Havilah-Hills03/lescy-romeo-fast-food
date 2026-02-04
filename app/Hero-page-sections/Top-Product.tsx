2db6472356309ea6c0ddee6aceb052ebb04580fe
Jerry Jakes <jerryjake227@gmail.com>
2026-02-04 15:25:08 +0100

    updated the card width and height

--- app/Hero-page-sections/Top-Product.tsx
+++ app/Hero-page-sections/Top-Product.tsx
@@ -71,9 +71,9 @@ const TopProduct: React.FC = () => {
      
  >
   <h1 className="h1 text-center pt-12 font-medium text-[20px] lg:text-[35px] md:text-[35px]" 
-  >Top Products</h1>
+  >Top Productsyy</h1>
      <div 
-      className=" max-w-4xl lg:max-w-8xl  md:max-w-7xl  flex justify-center items-center h-[48vh] lg:h-screen px-1"
+      className=" max-w-4xl lg:max-w-8xl  md:max-w-7xl  flex justify-center items-center h-[52vh] lg:h-screen px-1"
       
     >
       
